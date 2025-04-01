276ae09fc508e8bcbd9667619b53bf841b3a8434
vivek Koyani <vivek.koyani@gmail.com>
2025-04-01 22:20:58 +0530

    create separate files

---
+++ schemaGql.js
@@ -0,0 +1,25 @@
+import { gql } from 'apollo-server';
+
+const typeDefs = gql`
+type Query {
+  users: [User]
+  user(id:ID!):User
+  quotes: [Quote]
+  iquote(by:ID!): [Quote]
+}
+
+type User{
+id:ID
+firstName:String
+lastName:String
+email:String
+quotes: [Quote]
+}
+
+type Quote {
+  name: String
+  by: ID
+}
+`
+
+export default typeDefs
--- server.mjs
+++ server.mjs
@@ -1,39 +1,7 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
-import { quotes, users } from './fakedb.js';
-
-const typeDefs = gql`
-type Query {
-  users: [User]
-  user(id:ID!):User
-  quotes: [Quote]
-  iquote(by:ID!): [Quote]
-}
-
-type User{
-id:ID
-firstName:String
-lastName:String
-email:String
-quotes: [Quote]
-}
-
-type Quote {
-  name: String
-  by: ID
-}
-`
-const resolvers = {
-  Query: {
-    users: () => users,
-    user: (_, args) => users.find((user) => user.id === args.id) ,
-    quotes: () => quotes,
-    iquote: (_, args) => quotes.filter((quote) => quote.by === args.by),
-  },
-  User: {
-    quotes: (ur) => quotes.filter(quote => quote.by === ur.id) 
-  }
-}
+import resolvers from './resolvers.js';
+import typeDefs from './schemaGql.js';
 
 const server = new ApolloServer({
   typeDefs,
