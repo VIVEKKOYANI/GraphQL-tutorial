eba20d85595703c343db99cc84fbeea0c186ffb4
vivek Koyani <vivek.koyani@gmail.com>
2025-04-06 12:16:18 +0530

    add some dependencies and mongodb connection

--- server.mjs
+++ server.mjs
@@ -1,7 +1,21 @@
 import { ApolloServer } from 'apollo-server';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
-import resolvers from './resolvers.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import typeDefs from './schemaGql.js';
+import { MONGO_URI } from './config.js';
+
+// Load env variables
+dotenv.config();
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB Connected'))
+.catch((err) => console.log('MongoDB Connection Error:', err));
+
+import resolvers from './resolvers.js';
 
 const server = new ApolloServer({
   typeDefs,
