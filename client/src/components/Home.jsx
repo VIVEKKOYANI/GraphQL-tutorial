c3e4c27efc09da852f0c300eb677b646c8cde41f
vivek Koyani <vivek.koyani@gmail.com>
2025-04-19 00:01:38 +0530

    add get data for getAllQuotes

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import { useQuery } from '@apollo/client'
+import { GET_ALL_QUTOES } from '../gqloperations/queries'
 
 export default function Home() {
+  const { loading, error, data } = useQuery(GET_ALL_QUTOES);
+  console.log("data", data)
+  if (loading) return <h1>loading...</h1>
+  if (error) {
+    console.log("error", error.message);
+
+  }
   return (
     <div className='center-wrapper'>
       <div className="my-container">
-        <blockquote>
-          <h6>if it works dont touch it</h6>
-          <p className="right-align">~ram</p>
-        </blockquote>
-        <blockquote>
-          <h6>if it works dont touch it</h6>
-          <p className="right-align">~ram</p>
-        </blockquote>
+        {data?.quotes?.map((quote) => {
+          return (
+            <blockquote>
+              <h6>{quote.name}</h6>
+              <p className="right-align">~{quote.by.firstName}</p>
+            </blockquote>
+          )
+        })}
       </div>
     </div>
   )
