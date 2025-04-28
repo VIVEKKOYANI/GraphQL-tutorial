f00f577596f6ac9bd380624e6b938dedb984e9e8
vivek Koyani <vivek.koyani@gmail.com>
2025-04-28 20:16:32 +0530

    add signup mutation query

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
-import '../App.css'
 import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { SIGNUP_USER } from '../gqloperations/mutations';
+import '../App.css'
 
 function SignUp() {
   const [formData, setFormData] = useState({
     email: '',
     password: ''
   });
+  const [signupUser, {data, loading, error}] = useMutation(SIGNUP_USER)
 
   const handleChange = (e) => {
     setFormData({
@@ -17,12 +20,16 @@ function SignUp() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    signupUser(formData)
   }
+
+  if(loading) return <h1>Loading</h1>
 
   return (
     <div className="center-wrapper">
       <div className='my-container'>
+        {error && <div className='red card-panel'>{error.message}</div>}
+        {data && data.user && <div className='green card-panel'>{data.user.firstName} is SignedUp. You can login now!</div>}
         <h5>SignUp!!</h5>
         <form onSubmit={handleSubmit}>
         <input
